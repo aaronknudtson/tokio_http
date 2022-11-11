582db64e2d32e1dc2423798cd1635244683c7402
aaronknudtson <aaron.knudtson@outlook.com>
2022-11-11 17:00:21 -0500

    implemented start of Response

--- src/response.rs
+++ src/response.rs
@@ -14,5 +14,17 @@ impl Response {
         }
     }
 
-    pub fn write_status(&mut self, code: i32, status: &str)
+    pub async fn write_status(&mut self, code: i32, status: &str) -> Result<usize> {
+        self.writer.write(format!("HTTP/1.1 {} {}\n", code, status).as_bytes()).await
+    }
+    
+    pub async fn write_header(&mut self, key: &str, val: &str) -> Result<usize> {
+        self.writer.write(format!("\"{}\": {}\n", key, val).as_bytes()).await
+    }
+
+    pub async fn write_body(&mut self, val: &[u8]) -> Result<usize> {
+        self.write_header("content-length", val.len()).await?;
+        self.writer.write(b"\n").await?;
+        self.writer.write(val).await
+    }
 }
